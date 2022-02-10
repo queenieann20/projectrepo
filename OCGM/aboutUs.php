87a986776d0f21dbbe77520a1fd4787d803866e8
nieeeee <95670894+queenieann20@users.noreply.github.com>
2022-02-11 07:25:57 +0800

    Add files via upload

---
+++ OCGM/aboutUs.php
@@ -0,0 +1,87 @@
+<?php include("header1.php"); 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="aboutStyle.css">
+  
+</head>
+<body>
+
+<div class="card">
+  <div class="card-header">
+    <b>About Us</b>
+  </div>
+  <div class="card-body">
+    <h1> One Click Guiguinto Market </h1>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    
+  </div>
+</div>
+
+
+
+<div class="container mt-5">
+      <div class="row">
+        <div class="col-lg-4">
+          <form>
+            <div class="card" style="width: 18rem;">
+              <img src="pictures/gerF.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Geraldine Constantino</h5>
+                  <p class="card-text">Admin<br>constantinogeraldine5@gmail.com<br>09612718566</p>
+                  <a href="Contact Us.php" class="btn btn-primary">Contact Us</a>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-4">
+        <form>
+          <div class="card" style="width: 18rem;">
+            <img src="pictures/geoF.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Geonalyn Hernandez</h5>
+              <p class="card-text">Admin <br>geohrnndz22@gmail.com<br>09167507611</p>
+              <a href="Contact Us.php" class="btn btn-primary">Contact Us</a>
+            </div>
+          </div>
+        </form>
+        </div>
+          <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <img src="pictures/kwinsF.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Queenie Ann Dela Cruz</h5>
+                  <p class="card-text">Admin<br>delacruzqueenieann20@gmail.com<br>09277191899</p>
+                  <a href="Contact Us.php" class="btn btn-primary">Contact Us</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card" style="width: 18rem;">
+              <img src="pictures/sabF.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Christine Sabado</h5>
+                  <p class="card-text">Admin<br>istengsabado12@gmail.com<br> 09364610962</p>
+                  <a href="Contact Us.php" class="btn btn-primary">Contact Us</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+              <div class="card" style="width: 18rem;">
+                <img src="pictures/LudiF.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                      <h5 class="card-title">Ludilyn Duran</h5>
+                      <p class="card-text">Admin<br>ludyduran12@gmail.com<br>0906820695</p>
+                      <a href="Contact Us.php" class="btn btn-primary">Contact Us</a>
+                  
+                  </div>
+               </div>
+           </div>
+</div>
+</body>
+</html>
